f6d0e00626a5a7e17ddaecca74b1dfd7e7d90c9e
StephenIvan93 <69483625+StephenIvan93@users.noreply.github.com>
2020-12-08 23:03:45 +0000

    Assignment Submission #2

---
+++ Project2_Functions/Challenge #14/JS/main.js
@@ -0,0 +1,7 @@
+function My_Function() {        // Function
+    var name = prompt("Please enter your name");            // Prompt for name
+    name += " Sheppard";
+    document.getElementById("name").innerHTML = "Hello " + name + "!";  // Outputs Welcome followed by the name entered.
+}
+
+        
---
+++ Project2_Functions/JS/main.js
@@ -0,0 +1,6 @@
+function My_Function() {
+    var name = prompt("Please enter your name"); // Prompt for name.
+    document.getElementById("name").innerHTML = "Hello " + name + "!"; // Outputs name to screen
+}
+
+        
