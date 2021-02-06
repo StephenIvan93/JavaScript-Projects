fbc8e7dd11395a40b74dd559cfcd733cb56be943
StephenIvan93 <69483625+StephenIvan93@users.noreply.github.com>
2021-02-06 23:53:30 +0000

    Assignment Submission #5

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,93 @@
+//<------------------- Assignment #26 --------------------------->
+document.write("Assignment #26");
+document.write("<br>");   // Line Break
+document.write(typeof 5.7); // Outputs type as number
+document.write("<br>");   // Line Break
+
+//<------------------- Assignment #27 --------------------------->
+document.write("Assignment #27");
+document.write("<br>");   // Line Break
+document.write(2E10);     // Infinite number
+document.write("<br>");   // Line Break
+document.write(-2E10);    // Negative Infinite number
+document.write("<br>");   // Line Break
+
+//<------------------- Assignment #28 --------------------------->
+document.write("Assignment #28");
+document.write("<br>");   // Line Break
+document.write(2 > 5);    //Greater than Operator
+document.write("<br>");   // Line Break
+document.write( 5 == 5);  // Equal too Operator
+document.write("<br>");   // Line Break
+document.write(60 < 100); //less than operator
+document.write("<br>");   // Line Break
+
+//<------------------- Assignment #29 --------------------------->
+console.log(5 * 9);
+
+//<------------------- Assignment #30 --------------------------->
+document.write("Assignment #30");
+document.write("<br>");   // Line Break
+document.write("word" + 10);
+console.log("word" + 10);
+document.write("<br>");   // Line Break
+//<------------------- Challenge #7 --------------------------->
+console.log(100 >= 105);
+
+//<------------------- Assignment #31 --------------------------->
+document.write("Assignment #31");
+document.write("<br>");   // Line Break
+document.write( 5 == 5);  // Equal too Operator
+document.write("<br>");   // Line Break
+document.write( 20 == 5);  // Equal too Operator
+document.write("<br>");   // Line Break
+
+//<------------------- Assignment #32 --------------------------->
+document.write("Assignment #32");
+document.write("<br>");   // Line Break
+var a = 12;
+var a = 12;
+
+document.write(a === a);
+document.write("<br>");   // Line Break
+
+var x = "Hello";
+var y = 21;
+document.write(x === y);
+document.write("<br>");   // Line Break
+
+var g = '2021';
+var date = '2021';
+document.write(g === date);
+document.write("<br>");   // Line Break
+
+
+var length = 5;
+var age = 6;
+document.write(length === age);
+document.write("<br>");   // Line Break
+
+//<------------------- Assignment #33 --------------------------->
+document.write("Assignment #33");
+document.write("<br>");   // Line Break
+
+document.write(10 > 5 && 25> 20);   //Output True AND Operator
+document.write("<br>");   // Line Break
+document.write(10 < 5 && 25 < 20);   //Output False AND Operator
+document.write("<br>");   // Line Break
+document.write(10 > 5 || 25> 20);   //Output True AND Operator
+document.write("<br>");   // Line Break
+document.write(10 < 5 || 25 < 20);   //Output False AND Operator
+document.write("<br>");   // Line Break
+
+//<------------------- Assignment #34 --------------------------->
+document.write("Assignment #34");
+document.write("<br>");   // Line Break
+
+function not_Function(){
+  document.getElementById("Not").innerHTML =!(5 > 10);
+}
+document.write("<br>");   // Line Break
+function not_Fun(){
+  document.getElementById("NotFunc").innerHTML =!(20 > 10);
+}
